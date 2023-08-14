213c82dbcf7707e4f4f508f510b3e4e18abdd73e
CA-BiKen-789 <cabiken789@gmail.com>
2023-08-14 16:04:35 +0700

    add header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,7 +6,12 @@ const cx = classNames.bind(styles);
 function Header() {
 	return (
 		<div className={cx('wrapper')}>
-			Header
+			<h1>dashboard football</h1>
+			<img className={cx('logo')} src="/images/logo.png" alt="logo" />
+			<div className={cx('account')}>
+				<img src="/images/avatar.jpg" alt="avatar" />
+				<span>CA BiKen - IT Web</span>
+			</div>
 		</div>
 	);
 }
